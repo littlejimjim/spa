08d9c4d2a496a81e18fb13f878cb0785c3aedb0b
Kwok Hang Yee <20223277@life.hkbu.edu.hk>
2022-11-03 21:16:04 +0800

    Add vue router

---
+++ src/components/PaginatedBookings.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <table>
+      <tr v-for="booking in bookings" :key="booking._id">
+        <td>{{ booking._id }}</td>
+        <td>{{ booking.email }}</td>
+      </tr>
+    </table>
+    <button v-for="i in pages" :key="i" @click="fetchPage(i)">{{ i }}</button>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, computed } from "vue";
+
+export default {
+  name: 'PaginatedBookings',
+  props: {
+    msg: String
+  },
+  setup() {
+    const bookings = ref([]);
+    const lastPage = ref(0);
+    const perPage = ref(2);
+
+    const pages = computed(() => {
+      var pages = [];
+
+      for (var i = 1; i <= lastPage.value; i++) {
+        pages.push(i);
+      }
+
+      return pages;
+    });
+    const fetchPage = async function (page) {
+
+      var response = await fetch("/api/bookings?perPage=" + perPage.value + "&page=" + page);
+
+      if (response.ok) {
+        var data = await response.json();
+
+        bookings.value = data.bookings;
+        lastPage.value = data.pages
+
+      } else {
+        alert(response.statusText);
+      }
+    };
+    // onMounted(() => { fetchPage(1) });
+    onMounted(function () {
+      fetchPage(1);
+    });
+
+    return {
+      bookings,
+      pages,
+      fetchPage
+    };
+
+  },
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PaginatedBookings msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import PaginatedBookings from '@/components/PaginatedBookings.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    //HelloWorld
+    PaginatedBookings
   }
 }
 </script>
